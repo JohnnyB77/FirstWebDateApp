93915fff8a62f3e8ab7e9ecda4793e3d720664ec
John Boseman <jfboseman@hotmail.com>
2020-05-11 22:40:57 +0300

    Initial Commit

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(public authService: AuthService, private altertify: AlertifyService,
+              private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    /*console.log(this.model);*/
+    this.authService.login(this.model).subscribe(next => {
+      this.altertify.success('Logged in succesfully');
+    }, error => {
+      this.altertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
+    });
+  }
+
+  loggedIn() {
+    return this.authService.loggedIn();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.altertify.message('logged out');
+    this.router.navigate(['home']);
+  }
+
+}
